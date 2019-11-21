11a73280410700889d0759e402e3868f1452a897
Mathias <mathias@decodified.com>
2019-11-21 10:59:39 +0100

    Add LICENSE

--- addon/background.js
+++ addon/background.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019 Mathias Doenitz
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 const TITLE_ENABLE = "Enable triggered-reload";
 const TITLE_DISABLE = "Disable triggered-reload";
 
